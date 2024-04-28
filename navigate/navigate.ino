3b9642acbfc1c9baeb05978aba8d1c1c26edc18b
BellatheITgirl <107959376+BellatheITgirl@users.noreply.github.com>
2024-04-27 21:10:57 -0400

    motor driving

--- navigate/navigate.ino
+++ navigate/navigate.ino
@@ -14,70 +14,77 @@ Adafruit_MotorShield AFMStop(0x62); // top
 
 // Connect two steppers with 200 steps per revolution (1.8 degree)
 // to the top shield
-Adafruit_StepperMotor *myStepper1 = AFMStop.getStepper(200, 1);
-Adafruit_StepperMotor *myStepper2 = AFMStop.getStepper(200, 2);
+Adafruit_StepperMotor *top1 = AFMStop.getStepper(200, 1);
+Adafruit_StepperMotor *top2 = AFMStop.getStepper(200, 2);
 
 // Connect two steppers with 200 steps per revolution (1.8 degree)
 // to the middle shield
-Adafruit_StepperMotor *myStepper3 = AFMSmid.getStepper(200, 1);
-Adafruit_StepperMotor *myStepper4 = AFMSmid.getStepper(200, 2);
+Adafruit_StepperMotor *mid1 = AFMSmid.getStepper(200, 1);
+Adafruit_StepperMotor *mid2 = AFMSmid.getStepper(200, 2);
 
 // Connect two steppers with 200 steps per revolution (1.8 degree)
 // to the bottom shield
-Adafruit_StepperMotor *myStepper5 = AFMSbot.getStepper(200, 1);
-Adafruit_StepperMotor *myStepper6 = AFMSbot.getStepper(200, 2);
+Adafruit_StepperMotor *bot1 = AFMSbot.getStepper(200, 1);
+Adafruit_StepperMotor *bot2 = AFMSbot.getStepper(200, 2);
 
 // you can change these to DOUBLE or INTERLEAVE or MICROSTEP!
 // wrappers for the first motor!
 void forwardstep1() {
-  myStepper1->onestep(FORWARD, SINGLE);
+  top1->onestep(FORWARD, SINGLE);
 }
 void backwardstep1() {
-  myStepper1->onestep(BACKWARD, SINGLE);
+  top1->onestep(BACKWARD, SINGLE);
 }
 // wrappers for the second motor!
 void forwardstep2() {
-  myStepper2->onestep(FORWARD, DOUBLE);
+  top2->onestep(FORWARD, DOUBLE);
 }
 void backwardstep2() {
-  myStepper2->onestep(BACKWARD, DOUBLE);
+  top2->onestep(BACKWARD, DOUBLE);
 }
 // wrappers for the third motor!
 void forwardstep3() {
-  myStepper3->onestep(FORWARD, INTERLEAVE);
+  mid1->onestep(FORWARD, INTERLEAVE);
 }
 void backwardstep3() {
-  myStepper3->onestep(BACKWARD, INTERLEAVE);
+  mid1->onestep(BACKWARD, INTERLEAVE);
 }
 // wrappers for the fourth motor!
 void forwardstep4() {
-  myStepper4->onestep(FORWARD, SINGLE);
+  mid2->onestep(FORWARD, SINGLE);
 }
 void backwardstep4() {
-  myStepper4->onestep(BACKWARD, SINGLE);
+  mid2->onestep(BACKWARD, SINGLE);
 }
 // wrappers for the fifth motor!
 void forwardstep5() {
-  myStepper5->onestep(FORWARD, DOUBLE);
+  bot1->onestep(FORWARD, DOUBLE);
 }
 void backwardstep5() {
-  myStepper5->onestep(BACKWARD, DOUBLE);
+  bot1->onestep(BACKWARD, DOUBLE);
 }
 // wrappers for the sixth motor!
 void forwardstep6() {
-  myStepper6->onestep(FORWARD, INTERLEAVE);
+  bot2->onestep(FORWARD, INTERLEAVE);
 }
 void backwardstep6() {
-  myStepper6->onestep(BACKWARD, INTERLEAVE);
+  bot2->onestep(BACKWARD, INTERLEAVE);
 }
 
-// Now we'll wrap the 6 steppers in an AccelStepper object
-AccelStepper stepper1(forwardstep1, backwardstep1);
-AccelStepper stepper2(forwardstep2, backwardstep2);
-AccelStepper stepper3(forwardstep3, backwardstep3);
-AccelStepper stepper4(forwardstep4, backwardstep4);
-AccelStepper stepper5(forwardstep5, backwardstep5);
-AccelStepper stepper6(forwardstep6, backwardstep6);
+/*
+  leftFront
+  rightFront
+  leftBack
+  rightBack
+  frontScoop
+  backScoop
+  */
+AccelStepper frontScoop(forwardstep1, backwardstep1);
+AccelStepper backScoop(forwardstep2, backwardstep2);
+AccelStepper leftFront(forwardstep3, backwardstep3);
+AccelStepper leftBack(forwardstep4, backwardstep4);
+AccelStepper rightFront(forwardstep5, backwardstep5);
+AccelStepper rightBack(forwardstep6, backwardstep6);
 
 #define TCAADDR 0x70
 
@@ -131,15 +138,16 @@ void setup()
     Serial.println("\ndone");
 
   if(AFMSbot.begin()) {
-    Serial.println("Bottom motor found!");
+    Serial.println("Bottom motor shield found!");
   }
   if(AFMSmid.begin()) {
-    Serial.println("Middle motor found!");
+    Serial.println("Middle motor shield found!");
   }
   if(AFMStop.begin()) {
-    Serial.println("Top motor found!");
+    Serial.println("Top motor shield found!");
   }
 
+  /*
   stepper1.setMaxSpeed(100.0);
   stepper1.setAcceleration(100.0);
   stepper1.moveTo(24);
@@ -163,12 +171,34 @@ void setup()
   stepper6.setMaxSpeed(300.0);
   stepper6.setAcceleration(100.0);
   stepper6.moveTo(1000000);
+  */
     
   delay(2000);
 }
 
+// one step is 1.8 degrees with a 2 inch radius
+// so each step is .06 inches aka 1.6mm * .07 = 1.1mm forward per step
+
+void moveForward(int steps, int speed) {
+  leftFront.setSpeed(speed);
+  leftFront.moveTo(steps);
+  rightFront.setSpeed(speed);
+  rightFront.moveTo(steps);
+  leftBack.setSpeed(speed);
+  leftBack.moveTo(-steps);
+  rightBack.setSpeed(speed);
+  rightBack.moveTo(-steps);
+}
+
+void scoop() {
+  frontScoop.setSpeed(100);
+  frontScoop.moveTo(72);
+  
+}
+
 void loop() {
 
+  /*
    if (stepper1.distanceToGo() == 0) {
       stepper1.moveTo(-stepper1.currentPosition());
     }
@@ -198,109 +228,19 @@ void loop() {
   stepper4.run();
   stepper5.run();
   stepper6.run();
+  */
 
-  tcaselect(0);
-  Serial.println("Sensor 0:");
-  // Read sensor values (16 bit integers)
-  rgb[0][0] = RGB_sensors[0].readRed();
-  rgb[1][0] = RGB_sensors[0].readGreen();
-  rgb[2][0] = RGB_sensors[0].readBlue();
-    // Print out readings, change HEX to DEC if you prefer decimal output
-  Serial.print("Red: "); Serial.println(rgb[0][0],HEX);
-  Serial.print("Green: "); Serial.println(rgb[1][0],HEX);
-  Serial.print("Blue: "); Serial.println(rgb[2][0],HEX);
-  Serial.println();
-
-
-  tcaselect(1);
-  Serial.println("Sensor 1:");
-  // Read sensor values (16 bit integers)
-    rgb[0][1] = RGB_sensors[1].readRed();
-  rgb[1][1] = RGB_sensors[1].readGreen();
-  rgb[2][1] = RGB_sensors[1].readBlue();
-    // Print out readings, change HEX to DEC if you prefer decimal output
-  Serial.print("Red: "); Serial.println(rgb[0][1],HEX);
-  Serial.print("Green: "); Serial.println(rgb[1][1],HEX);
-  Serial.print("Blue: "); Serial.println(rgb[2][1],HEX);
-  Serial.println();
-
-
-  tcaselect(2);
-  Serial.println("Sensor 2:");
-  // Read sensor values (16 bit integers)
-  rgb[0][2] = RGB_sensors[2].readRed();
-  rgb[1][2] = RGB_sensors[2].readGreen();
-  rgb[2][2] = RGB_sensors[2].readBlue();
-    // Print out readings, change HEX to DEC if you prefer decimal output
-  Serial.print("Red: "); Serial.println(rgb[0][2],HEX);
-  Serial.print("Green: "); Serial.println(rgb[1][2],HEX);
-  Serial.print("Blue: "); Serial.println(rgb[2][2],HEX);
-  Serial.println();
-
-
-  tcaselect(3);
-  Serial.println("Sensor 3:");
-  // Read sensor values (16 bit integers)
-  rgb[0][3] = RGB_sensors[3].readRed();
-  rgb[1][3] = RGB_sensors[3].readGreen();
-  rgb[2][3] = RGB_sensors[3].readBlue();
-    // Print out readings, change HEX to DEC if you prefer decimal output
-  Serial.print("Red: "); Serial.println(rgb[0][3],HEX);
-  Serial.print("Green: "); Serial.println(rgb[1][3],HEX);
-  Serial.print("Blue: "); Serial.println(rgb[2][3],HEX);
-  Serial.println();
-
-
-  tcaselect(4);
-  Serial.println("Sensor 4:");
-  // Read sensor values (16 bit integers)
-  rgb[0][4] = RGB_sensors[4].readRed();
-  rgb[1][4] = RGB_sensors[4].readGreen();
-  rgb[2][4] = RGB_sensors[4].readBlue();
-    // Print out readings, change HEX to DEC if you prefer decimal output
-  Serial.print("Red: "); Serial.println(rgb[0][4],HEX);
-  Serial.print("Green: "); Serial.println(rgb[1][4],HEX);
-  Serial.print("Blue: "); Serial.println(rgb[2][4],HEX);
-  Serial.println();
-
-
-  tcaselect(5);
-  Serial.println("Sensor 5:");
-  // Read sensor values (16 bit integers)
-  rgb[0][5] = RGB_sensors[5].readRed();
-  rgb[1][5] = RGB_sensors[5].readGreen();
-  rgb[2][5] = RGB_sensors[5].readBlue();
-    // Print out readings, change HEX to DEC if you prefer decimal output
-  Serial.print("Red: "); Serial.println(rgb[0][5],HEX);
-  Serial.print("Green: "); Serial.println(rgb[1][5],HEX);
-  Serial.print("Blue: "); Serial.println(rgb[2][5],HEX);
-  Serial.println();
-
-
-  tcaselect(6);
-  Serial.println("Sensor 6:");
-  // Read sensor values (16 bit integers)
-  rgb[0][6] = RGB_sensors[6].readRed();
-  rgb[1][6] = RGB_sensors[6].readGreen();
-  rgb[2][6] = RGB_sensors[6].readBlue();
-    // Print out readings, change HEX to DEC if you prefer decimal output
-  Serial.print("Red: "); Serial.println(rgb[0][6],HEX);
-  Serial.print("Green: "); Serial.println(rgb[1][6],HEX);
-  Serial.print("Blue: "); Serial.println(rgb[2][6],HEX);
-  Serial.println();
-
-
-  tcaselect(7);
-  Serial.println("Sensor 7:");
-  // Read sensor values (17 bit integers)
-  rgb[0][7] = RGB_sensors[7].readRed();
-  rgb[1][7] = RGB_sensors[7].readGreen();
-  rgb[2][7] = RGB_sensors[7].readBlue();
-    // Print out readings, change HEX to DEC if you prefer decimal output
-  Serial.print("Red: "); Serial.println(rgb[0][7],HEX);
-  Serial.print("Green: "); Serial.println(rgb[1][7],HEX);
-  Serial.print("Blue: "); Serial.println(rgb[2][7],HEX);
-  Serial.println();
+  for(int i = 0; i < 8; i++) {
+    tcaselect(i);
+    Serial.print("Sensor "); Serial.print(i); Serial.println(":");
+    rgb[0][i] = RGB_sensors[i].readRed();
+    rgb[1][i] = RGB_sensors[i].readGreen();
+    rgb[2][i] = RGB_sensors[i].readBlue();
+    Serial.print("Red: "); Serial.println(rgb[0][i],HEX);
+    Serial.print("Green: "); Serial.println(rgb[1][i],HEX);
+    Serial.print("Blue: "); Serial.println(rgb[2][i],HEX);
+    Serial.println();
+  }
 
   delay(2000);
   
