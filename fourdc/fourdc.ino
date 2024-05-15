85abe63d19a1269f8f1f22675e898bafb5554c16
BellatheITgirl <107959376+BellatheITgirl@users.noreply.github.com>
2024-05-15 16:06:11 -0400

    truth table & calibration

--- fourdc/fourdc.ino
+++ fourdc/fourdc.ino
@@ -8,8 +8,9 @@
 #define echoPin 12
 
 bool seesRed;
+bool seesSilver;
 int P = 0.5; // CALIBRATE THIS
-int speed = 150;
+int speed = 50;
 
 int counter;
 
@@ -20,12 +21,12 @@ float distance;
 SFE_ISL29125 RGB_sensors[8];
 
 /*
-  Wiring Colors
-    White - Ground
-    Purple - 5V
-    Red - SCL
-    Green - SDA
-    Gray - 3.3V
+  PCB Wiring Colors
+    White - 5V
+    Purple - SDA
+    Black - SCL
+    Green - Ground
+    Blue - 3.3V
 */
 
 const int front = 4;
@@ -35,6 +36,7 @@ const int leftBack = 5;
 const int rightBack = 7;
 const int leftPID = 1;
 const int rightPID = 0;
+const int back = 3;
 
 unsigned int rgb[3][8];
 
@@ -90,22 +92,60 @@ void getRGBs() {
   }
 }
 
-void leftDrive(int vel) {
-  analogWrite(enAL, vel);
-  analogWrite(enBL, vel);
-  digitalWrite(in1L, LOW);
-  digitalWrite(in2L, HIGH);
-  digitalWrite(in3L, LOW);
-  digitalWrite(in4L, HIGH);
+void leftFwd(int vel) {
+  if(vel < 0) {
+    leftBkd(abs(vel));
+  }
+  else {
+    analogWrite(enAL, vel);
+    analogWrite(enBL, vel);
+    digitalWrite(in1L, LOW);
+    digitalWrite(in2L, HIGH);
+    digitalWrite(in3L, LOW);
+    digitalWrite(in4L, HIGH);
+  }
 }
 
-void rightDrive(int vel) {
-  analogWrite(enAR, vel);
-  analogWrite(enBR, vel);
-  digitalWrite(in1R, LOW);
-  digitalWrite(in2R, HIGH);
-  digitalWrite(in3R, LOW);
-  digitalWrite(in4R, HIGH);
+void rightFwd(int vel) {
+  if(vel < 0) {
+    rightBkd(abs(vel));
+  }
+  else {
+    analogWrite(enAR, vel);
+    analogWrite(enBR, vel);
+    digitalWrite(in1R, LOW);
+    digitalWrite(in2R, HIGH);
+    digitalWrite(in3R, LOW);
+    digitalWrite(in4R, HIGH);
+  }
+}
+
+void leftBkd(int vel) {
+  if(vel < 0) {
+    leftFwd(abs(vel));
+  }
+    else {
+    analogWrite(enAL, vel);
+    analogWrite(enBL, vel);
+    digitalWrite(in1L, HIGH);
+    digitalWrite(in2L, LOW);
+    digitalWrite(in3L, HIGH);
+    digitalWrite(in4L, LOW);
+  }
+}
+
+void rightBkd(int vel) {
+  if(vel > 0) {
+    rightFwd(abs(vel));
+  }
+  else {
+    analogWrite(enAR, vel);
+    analogWrite(enBR, vel);
+    digitalWrite(in1R, HIGH);
+    digitalWrite(in2R, LOW);
+    digitalWrite(in3R, HIGH);
+    digitalWrite(in4R, LOW);
+  }
 }
 
 void stop() {
@@ -119,6 +159,7 @@ void stop() {
   digitalWrite(in4L, LOW);
 }
 
+/*
 int checkColor(int sensor) {
   if((rgb[0][sensor] > 40000) && (rgb[1][sensor] > 40000) && (rgb[2][sensor] > 30000)) {
     return 6; // white
@@ -133,8 +174,35 @@ int checkColor(int sensor) {
     return 5; // red
     seesRed = true;
   }
+  else if((rgb[0][sensor] > 15000) && (rgb[1][sensor] < 15000) && (rgb[2][sensor] < 7000)) { //RECODE TO SILVER
+    return 7; //silver
+    seesSilver = !seesSilver;
+  }
+  else return 0;
+}*/
+int checkColor(int sensor) {
+  if((rgb[0][sensor] > 1500) && (rgb[1][sensor] > 1500)) {
+    return 6; // white
+  }
+  else if((rgb[0][sensor] < 1500) && (rgb[1][sensor] < 1500)) {
+    return 1; // black
+  }
+  else if((rgb[0][sensor] < 1500) && (rgb[1][sensor] > 1500)) {
+    return 3; // green
+  }
+  else if((rgb[0][sensor] > 1500) && (rgb[1][sensor] < 1500)) {
+    return 5; // red
+    seesRed = true;
+  }
+  /*
+  else if((rgb[0][sensor] > 15000) && (rgb[1][sensor] < 15000) && (rgb[2][sensor] < 7000)) { //RECODE TO SILVER
+    return 7; //silver
+    seesSilver = !seesSilver;
+  }
+  */
   else return 0;
 }
+
 
 int takeAve(int sensor1, int sensor2) {
   int r = rgb[0][sensor1] - rgb[0][sensor2];
@@ -161,26 +229,221 @@ float getDistance()
   return calculatedDistance;              //send back the distance that was calculated
 }
 
+void leftTurn() {
+  rightFwd(speed);
+  leftBkd(speed);
+  delay((1000 / 75) * speed);
+  stop();
+}
+
+void rightTurn() {
+  leftFwd(speed);
+  rightBkd(speed);
+  delay((1000 / 75) * speed);
+  stop();
+}
+
+void convertToInt() {
+  values[0] = checkColor(rightBack);
+  values[1] = checkColor(leftBack);
+  values[2] = checkColor(rightMid);
+  values[3] = checkColor(leftMid);
+  values[4] = checkColor(front);
+}
+
+bool truth() { // 1984
+  if(values[0] == 1) {
+    //front sensor reads black
+    if(values[1] == 1) {
+      //left mid reads black
+      if(values[2] == 1) {
+        //right mid reads black
+        if((values[3] == 6) && (values[4] == 3)) {
+          //left rear reads white
+          //right rear reads green
+          // BBBWG
+          rightTurn();
+          return true;
+        }
+        else if(values[3] == 3) {
+          //left rear reads green
+          if(values[4] == 6) {
+            //right rear reads white
+            // BBBGW
+            leftTurn();
+            return true;
+          }
+          else if(values[4] == 3) {
+            //right rear reads green
+            // BBBGG
+            rightTurn();
+            rightTurn();
+            return true;
+          }
+        }
+      }
+      else if((values[2] == 6) && (values[3] == 3) && (values[4] == 6)) {
+        //right mid reads white
+        //left rear reads green
+        //right rear reads white
+        // BBWGW
+        leftTurn();
+        return true;
+      }
+    }
+    else if((values[1] == 6) && (values[2] == 1) && (values[3] == 6) && (values[4] == 3)) {
+      //left mid reads white
+      //right mid reads black
+      //left rear reads white
+      //right rear reads green
+      // BWBWG
+      rightTurn();
+      return true;
+    }
+  }
+  else if(values[0] == 6) {
+    //front sensor reads white
+    if(values[1] == 1) {
+      //left mid reads black
+      if(values[2] == 1) {
+        //right mid reads black
+        if(values[3] == 6) {
+          //left rear reads white
+          if(values[4] == 6) {
+            //right rear reads white
+            // WBBWW
+            // Initialization
+            return true;
+          }
+          else if(values[4] == 3) {
+            //right rear reads green
+            // WBBWG
+            rightTurn();
+            return true;
+          }
+        }
+        else if(values[3] == 3) {
+          //left rear reads green
+          if(values[4] == 6) {
+            //right rear reads white
+            // WBBGW
+            leftTurn();
+            return true;
+          }
+          else if(values[4] == 3) {
+            //right rear reads green
+            // WBBGG
+            rightTurn();
+            rightTurn();
+            return true;
+          }
+        }
+      }
+      else if(values[2] == 6) {
+        //right mid reads white
+        if((values[3] == 1) && (values[4] == 6)) {
+          //left rear reads black
+          //right rear reads white
+          // WBWBW
+          // Shift left
+          return true;
+        }
+        else if(values[3] == 6) {
+          //left rear reads white
+          if(values[4] == 1) {
+            //right rear reads black
+            // WBWWB
+            // Turn left (~60 deg)
+            return true;
+          }
+          else if(values[4] == 6) {
+            //right rear reads white
+            // WBWWW
+            // Check rear sensor
+            return true;
+          }
+        }
+        else if((values[3] == 3) && (values[4] == 6)) {
+          //left rear reads green
+          //right rear reads white
+          // WBWGW
+          leftTurn();
+          return true;
+        }
+      }
+    }
+    else if(values[1] == 6) {
+      //left mid reads white
+      if(values[2] == 1) {
+        //right mid reads black
+        if((values[3] == 1) && (values[4] == 6)) {
+          //left rear reads black
+          //right rear reads white
+          // WWBBW
+          // Turn right (60 deg)
+          return true;
+        }
+        else if(values[3] == 6) {
+          //left rear reads white
+          if(values[4] == 1) {
+            //right rear reads black
+            // WWBWB
+            // Shift right
+            return true;
+          }
+          else if(values[4] == 6) {
+            //right rear reads white
+            // WWBWW
+            // Check rear sensor
+            return true;
+          }
+          else if(values[4] == 3) {
+            //right rear reads green
+            // WWBWG
+            rightTurn();
+            return true;
+          }
+        }
+      }
+      else if((values[2] == 6) && (values[3] == 6) && (values[4] == 6)) { //hope didnt break anything here, swaped %% with &&. compiler error :(
+        //right mid reads white
+        //left rear reads white
+        //right rear reads white
+        // WWWWW
+        // Straight with a timer
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 void linefollowing() {
+  int aveDif = takeAve(leftPID, rightPID);
+  //Serial.println(aveDif);
+  leftFwd(speed + (aveDif * P));
+  rightFwd(speed - (aveDif * P));
+  /*
   if(checkColor(front) == 1) {
     int aveDif = takeAve(leftPID, rightPID);
-    leftDrive(speed + (aveDif * P));
-    rightDrive(speed - (aveDif * P));
+    leftFwd(speed + (aveDif * P));
+    rightFwd(speed - (aveDif * P));
   }
   else if(checkColor(front) == 6) {
     if((checkColor(leftPID) == 1) && (checkColor(rightPID) == 6)) {
-      leftDrive(speed);
-      rightDrive(speed / 2);
+      leftFwd(speed);
+      rightFwd(speed / 2);
     }
     else if((checkColor(leftPID) == 6) && (checkColor(rightPID) == 1)) {
-      leftDrive(speed / 2);
-      rightDrive(speed);
+      leftFwd(speed / 2);
+      rightFwd(speed);
     }
     else if((checkColor(leftPID) == 1) && (checkColor(rightPID) == 1)) {
-      leftDrive(speed);
-      rightDrive(speed);
+      leftFwd(speed);
+      rightFwd(speed);
     }
   }
+  */
 }
 
 void setup() {
@@ -226,30 +489,57 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   distance = getDistance();
+  //Serial.println(distance);
+  //Serial.print("Left: "); Serial.print(rgb[0][leftPID]); Serial.print(" "); Serial.print(rgb[1][leftPID]); Serial.print(" "); Serial.println(rgb[3][leftPID]);
+  //Serial.print("Right: "); Serial.print(rgb[0][rightPID]); Serial.print(" "); Serial.print(rgb[1][rightPID]); Serial.print(" "); Serial.println(rgb[3][rightPID]);
   getRGBs();
   checkColor(front);
-  if(seesRed || (distance < 10) || (counter > 1000)) {
+  //Serial.print(rgb[0][front]); Serial.print(" "); Serial.print(rgb[1][front]); Serial.print(" "); Serial.println(rgb[3][front]);
+  Serial.println(checkColor(front));
+  Serial.print(checkColor(leftPID));Serial.print(" ");
+  Serial.println(checkColor(rightPID));
+  
+  if(seesRed) {
+    stop();
+  }
+  else if(distance < 10) {
+    //go around
     stop();
   }
   else {
-    linefollowing();
-    //leftDrive(speed);
-    //rightDrive(speed);
+    /*
+    if(checkColor(front) == 6) {
+      leftFwd(speed);
+      rightFwd(speed);
+    }
+    else if(checkColor(front) == 1) {
+      stop();
+    }
+    */
+    if(!truth()) {
+      linefollowing();
+    }
   }
-  counter++;
-  delay(10);
+
+  //delay(100);
+
+  //leftBkd(speed);
+  //rightBkd(speed);
+  
+  //counter++;
+  //delay(10);
 
   /*  
   for(int i = 25; i < 256; i++) {
-    leftDrive(i);
-    rightDrive(i);
+    leftFwd(i);
+    rightFwd(i);
     delay(100);
   }
   delay(2000);
 
   for(int i = 25; i < 256; i++) {
-    leftDrive(-i);
-    rightDrive(-i);
+    leftBkd(i);
+    rightBkd(i);
     delay(100);
   }
   delay(2000);
